054c0de834fbf00a0ec16859ddfb954e6e8d2895
Samiylo Kryshu <skryshu@gmail.com>
2020-03-30 18:51:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Dog 
+  
+  def name=(dog_name)
+    @this_dog = dog_name
+  end
+  
+  def name
+    @this_dog
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
+
