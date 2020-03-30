fd87499b50283e113b3fd33d47627b40e70c23cf
Samiylo Kryshu <skryshu@gmail.com>
2020-03-30 18:50:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Dog 
+  
+  def name=(dog_name)
+    @this_dog = dog_name
+  end
+  
+  def name
+    puts @this_dog
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
+
